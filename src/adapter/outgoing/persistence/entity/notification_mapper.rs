e5daaa204d2d2875f0d2d64dd3cc050183f1663c
pereira <carloscdap@gmail.com>
2025-03-09 10:54:54 +0000

    Improve mapper

--- src/adapter/outgoing/persistence/entity/notification_mapper.rs
+++ src/adapter/outgoing/persistence/entity/notification_mapper.rs
@@ -14,6 +14,19 @@ impl From<Notification> for NotificationEntity {
     }
 }
 
+impl From<NotificationEntity> for Notification {
+    fn from(notification_entity: NotificationEntity) -> Self {
+        Self {
+            uuid: notification_entity.uuid,
+            message: notification_entity.message,
+            seen: notification_entity.seen,
+            deleted: notification_entity.deleted,
+            date_created: notification_entity.date_created,
+            last_updated: notification_entity.last_updated,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +43,28 @@ mod tests {
         assert_eq!(entity.deleted, notification.deleted);
         assert_eq!(entity.date_created, notification.date_created);
         assert_eq!(entity.last_updated, notification.last_updated);
+    }
+
+    #[test]
+    fn test_notification_entity_to_notification_mapping() {
+        let expected_notification = Notification::new("some message".to_string());
+
+        let notification_entity = NotificationEntity {
+            uuid: expected_notification.uuid,
+            message: expected_notification.message,
+            seen: false,
+            deleted: false,
+            date_created: expected_notification.date_created,
+            last_updated: expected_notification.last_updated,
+        };
+
+        let result: Notification = notification_entity.clone().into();
+
+        assert_eq!(result.uuid, expected_notification.uuid);
+        assert_eq!(result.message, "some message");
+        assert_eq!(result.seen, expected_notification.seen);
+        assert_eq!(result.deleted, expected_notification.deleted);
+        assert_eq!(result.date_created, expected_notification.date_created);
+        assert_eq!(result.last_updated, expected_notification.last_updated);
     }
 }
